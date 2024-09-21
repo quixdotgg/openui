9ce1518a42f7e8095e110f267ee37ab21b49347b
mabroorcc <mabroorcc@gmail.com>
2024-09-21 21:27:46 +0500

    fixed darkmode light mode

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -25,16 +25,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
+        <ThemeProvider attribute="class">
           {children}
         </ThemeProvider>
       </body>
--- website/src/components/ThemeProvider.tsx
+++ website/src/components/ThemeProvider.tsx
@@ -1,9 +1,35 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { type ThemeProviderProps } from "next-themes/dist/types";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  const [theme, setTheme] = React.useState<"light" | "dark">();
+
+  // Detect user's system preference for dark or light mode
+  React.useEffect(() => {
+    const prefersDarkMode = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    setTheme(prefersDarkMode ? "dark" : "light");
+
+    // Listen for changes in the system preference
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    const handleChange = (e: MediaQueryListEvent) => {
+      setTheme(e.matches ? "dark" : "light");
+    };
+    mediaQuery.addEventListener("change", handleChange);
+
+    // Cleanup event listener on unmount
+    return () => {
+      mediaQuery.removeEventListener("change", handleChange);
+    };
+  }, []);
+
+  return (
+    <NextThemesProvider {...props} forcedTheme={theme}>
+      {children}
+    </NextThemesProvider>
+  );
 }
