5648f98618ea84e8b45d87f6d9dbcf6883d451ef
mabroorcc <mabroorcc@gmail.com>
2024-09-21 21:09:43 +0500

    added schadcn theme

--- website/src/app/UserView.tsx
+++ website/src/app/UserView.tsx
@@ -1,22 +1,25 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { User } from "./types";
+import { Card, CardContent } from "@/components/ui/card";
 
 export function UserView({ user }: { user: User }) {
   return (
-    <div className="flex items-center space-x-4 p-4 border rounded-md">
-      <Avatar>
-        {user.image && <AvatarImage src={user.image} />}
-        {user.name && (
-          <AvatarFallback>
-            {user.name.charAt(0).toUpperCase() +
-              user.name.charAt(1).toUpperCase()}
-          </AvatarFallback>
-        )}
-      </Avatar>
-      <div>
-        <p className="text-sm font-medium leading-none">{user.name}</p>
-        <p className="text-sm text-muted-foreground">{user.email}</p>
-      </div>
-    </div>
+    <Card className="pt-6">
+      <CardContent className="flex gap-2 items-center">
+        <Avatar>
+          {user.image && <AvatarImage src={user.image} />}
+          {user.name && (
+            <AvatarFallback>
+              {user.name.charAt(0).toUpperCase() +
+                user.name.charAt(1).toUpperCase()}
+            </AvatarFallback>
+          )}
+        </Avatar>
+        <div>
+          <p className="text-sm font-medium leading-none">{user.name}</p>
+          <p className="text-sm text-muted-foreground">{user.email}</p>
+        </div>
+      </CardContent>
+    </Card>
   );
 }
