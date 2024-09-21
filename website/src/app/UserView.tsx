b9a6843d3bc0ddd98f1db684bee285bb4d44c1a8
mabroorcc <mabroorcc@gmail.com>
2024-09-21 18:23:39 +0500

    added github auth, Userview

---
+++ website/src/app/UserView.tsx
@@ -0,0 +1,22 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { User } from "./types";
+
+export function UserView({ user }: { user: User }) {
+  return (
+    <div className="flex items-center space-x-4 p-4 border rounded-md w-64">
+      <Avatar>
+        {user.image && <AvatarImage src={user.image} />}
+        {user.name && (
+          <AvatarFallback>
+            {user.name.charAt(0).toUpperCase() +
+              user.name.charAt(1).toUpperCase()}
+          </AvatarFallback>
+        )}
+      </Avatar>
+      <div>
+        <p className="text-sm font-medium leading-none">{user.name}</p>
+        <p className="text-sm text-muted-foreground">{user.email}</p>
+      </div>
+    </div>
+  );
+}
---
+++ website/src/auth.ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [
+    GitHub({
+      authorization: {
+        params: {
+          redirect_uri: process.env.AUTH_GITHUB_REDIRECT,
+        },
+      },
+    }),
+  ],
+});
